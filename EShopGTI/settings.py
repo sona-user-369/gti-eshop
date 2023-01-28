00fcf1f77e5819ec6e3bcf117c933f52282b9a08
donadavak <donadavak@gmail.com>
2023-01-28 02:16:48 +0100

    update for hosting

--- EShopGTI/settings.py
+++ EShopGTI/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'EShopGTI.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DataGTI',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'DataGTI',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    # }
 }
 
 # DATABASES = {
